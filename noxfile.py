bd23dc003bb06f15c89b59ee9c21c11abe20251e
lewoudar <lewoudar@gmail.com>
2020-11-04 20:02:53 +0100

    noxfile.py: added 3.9 to the list of python interpreters ci: commented line to limit concurrent jobs

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
-PYTHON_VERSIONS = ['pypy3', '3.6', '3.7', '3.8']
+PYTHON_VERSIONS = ['pypy3', '3.6', '3.7', '3.8', '3.9']
 
 
 @nox.session(python=PYTHON_VERSIONS[-1])
